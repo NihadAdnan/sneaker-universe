dd31ecf514295095b3f15a33e296fe6edf441065
NIhad Adnan Shah <nihad08adnan@gmail.com>
2024-06-21 21:19:20 +0600

    Update server.js

--- server.js
+++ server.js
@@ -6,10 +6,14 @@ import authRoutes from "./routes/authRoute.js";
 import cors from "cors";
 import productRoutes from "./routes/productRoutes.js";
 import path from "path";
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
 connectDB();
+
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 
 const app = express();
 
